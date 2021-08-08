0cb1a1f6f5bb9e50298cdd98f2c478025712c4bd
gn-grc <genejogarcia.gg@gmail.com>
2021-08-08 16:57:50 +0800

    implemented sign up functions with database

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,24 @@
+// Models
+const User = require("mongoose").model("User");
+
 exports.signin = (req, res, next) => {
   res.send("sign in");
 };
 
-exports.signup = (req, res, next) => {
-  res.send("sign up");
+exports.signup = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const newUser = await User.create({ email, password });
+
+    res.status(200).send({
+      success: true,
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message,
+    });
+  }
 };
