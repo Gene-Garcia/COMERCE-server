2125f63771dd201272b507e348f2720bb366d446
gn-grc <genejogarcia.gg@gmail.com>
2021-08-09 21:47:53 +0800

    created send grid mailer utility

--- routes/test.js
+++ routes/test.js
@@ -6,9 +6,26 @@ const { authorize } = require("../middleware/auth");
 
 // controller
 
+// utils
+const { sendMailer } = require("../utils/mailer");
+
 // Routes
 route.get("/private", authorize, (req, res, next) => {
   res.send("Authorize");
+});
+
+route.get("/mail", async function (req, res) {
+  const msg = {
+    to: "gjgarcia@live.mcl.edu.ph", // Change to your recipient
+    from: "genejogarcia.gg@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+
+  const result = await sendMailer(msg);
+  console.log("test.js " + result);
+  res.json(result);
 });
 
 module.exports = route;
---
+++ utils/mailer.js
@@ -0,0 +1,28 @@
+// SendGrid Web API
+
+// Package
+require("dotenv").config();
+const mailer = require("@sendgrid/mail");
+
+exports.sendMailer = async (message) => {
+  // Format
+  /*
+    to: 'test@example.com', // Change to your recipient
+    from: 'test@example.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    */
+
+  // Configuration
+  mailer.setApiKey(process.env.SENDGRID_API_KEY);
+
+  try {
+    const result = await mailer.send(message);
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+    return error.message;
+  }
+};
