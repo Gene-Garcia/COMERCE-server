00b045e97f85bfe6189028bc465ee504caa7f098
gn-grc <genejogarcia.gg@gmail.com>
2021-08-27 20:02:10 +0800

    changed uri, and made async await connection

--- config/database.js
+++ config/database.js
@@ -3,11 +3,30 @@ require("dotenv").config();
 // Packages
 const mongoose = require("mongoose");
 
-// Connect
-mongoose.connect(`${process.env.DB_URI}/${process.env.DB_NAME}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@personal-cluster.sofzm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Connect, async
+(async () => {
+  try {
+    await mongoose
+      .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(
+        () => {
+          console.info(`Connected to database`);
+        },
+        (error) => {
+          console.error(`Connection error: ${error.stack}`);
+        }
+      );
+  } catch (error) {
+    console.log("error");
+    console.log(error);
+  }
+})();
 
 mongoose.set("useCreateIndex", true);
 
