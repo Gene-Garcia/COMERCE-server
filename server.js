b417007c278908e665c9b6aad12b521a4916a62e
gn-grc <genejogarcia.gg@gmail.com>
2021-09-18 22:01:07 +0800

    removed requiring cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 // Packages
 const express = require("express");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const csrf = require("csurf");
 
