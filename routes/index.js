a9e46cb9f241604b16911272f77bb1be2a155e6a
gn-grc <genejogarcia.gg@gmail.com>
2021-09-18 15:13:28 +0800

    implemented server-dependent setting of csrf token instead setting in frontend

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,14 @@ router.get("/cs", (req, res) => {
   // the following commented lines of codes was attempted to set the csrf token through the server, but was failure.
   // it really needed to be set by the frontent API caller like axios
   // res.cookie("csrfcmrc", csrf);
-  // res.cookie("X-CSRF-Token", csrf, { httpOnly: true });
-  res.status(200).json({ csrfToken: csrf });
+  // res.cookie("X-CSRF-Token", csrf);
+  // res.cookie("_csrf", csrf);
+
+  // however, if we were to print the axios.defaults in the frontend
+  // it will show that the cookie name for csrf token they use is 'XSRF-TOKEN'
+  // which definitely works even when we dont set it manually in the header.
+  res.cookie("XSRF-TOKEN", csrf);
+  res.status(200).json();
 });
 
 module.exports = router;
