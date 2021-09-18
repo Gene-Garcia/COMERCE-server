a7830822a7ba4b4c56fa3d0250c77cac8032879c
gn-grc <genejogarcia.gg@gmail.com>
2021-09-18 15:12:45 +0800

    uninstall cors because it is not recommended used with csurf

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 /* A determiner variable that will help the server to know where the API requests will come from */
 const isProduction = process.env.PRODUCTION || false;
-conts = origin = isProduction
+const origin = isProduction
   ? "https://co-merce.netlify.app"
   : "http://localhost:3000";
 
@@ -19,28 +19,14 @@ conts = origin = isProduction
 require("./config/database");
 
 // Middlewares
-app.set("trust proxy", 1); // ref: https://stackoverflow.com/questions/66503751/cross-domain-session-cookie-express-api-on-heroku-react-app-on-netlify
-app.use(express.json());
 app.use(cookieParser());
 /*
  * Applying this middleware sets cookie in res with the cookie, on the first request and updates it once the token expires,
  * token which will be use to verify every request (GET, POST, etc.) made to the server.
  */
 app.use(csrf({ cookie: true }));
-/*
- * Setting the credentials to true makes the server expect the use of cookies that holds secure token,
- * or any cookies for that matter.
- *
- * Setting the origin is crucial to further enforce security and the csrf.
- * The server will only handle requests made from the set origin, hence, any request made
- * by other sites (XSS or cross-site attacks) will not be handled and will not be given a CSRF token
- */
-app.use(
-  cors({
-    credentials: true,
-    origin: origin,
-  })
-);
+app.set("trust proxy", 1); // ref: https://stackoverflow.com/questions/66503751/cross-domain-session-cookie-express-api-on-heroku-react-app-on-netlify
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", origin);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH");
