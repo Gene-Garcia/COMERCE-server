bf3abfc730b5a9e4f57ede927d5a4853e44c6831
gn-grc <genejogarcia.gg@gmail.com>
2021-10-06 00:01:47 +0800

    added controller function to get the unrated products ordered by the user

---
+++ controller/rate.js
@@ -0,0 +1,56 @@
+// models
+const Order = require("mongoose").model("Order");
+
+// constants
+const { error } = require("../config/errorMessages");
+const { orderStatus } = require("../config/status");
+
+/*
+ * GET Method
+ *
+ */
+exports.getUserToRateProduct = async (req, res, next) => {
+  const userId = "6127b3b64dfdba29d40a561b";
+
+  try {
+    // get user's order that has a status of Review
+    // populate orderedProducts that has rate === true
+    // get those products
+    let orders = await Order.find(
+      {
+        _customer: userId,
+        status: orderStatus[2],
+      },
+      "status orderDate ETADate orderedProducts.rated orderedProducts._product"
+    ).populate({
+      path: "orderedProducts._product",
+      select: "item imageAddress",
+    });
+
+    // flattens the data into client-ready object
+    // orderId and dates of the order are redundantly stored in each products
+    // so that client can easily access the order information of each product
+    let products = [];
+    orders.forEach((e) => {
+      const base = {
+        orderId: e._id,
+        orderDate: e.orderDate,
+        ETADate: e.ETADate,
+      };
+
+      e.orderedProducts.forEach(({ _product: f, rated }) => {
+        if (!rated)
+          products.push({
+            ...base,
+            item: f.item,
+            imageAddress: f.imageAddress,
+          });
+      });
+    });
+
+    res.status(200).json({ products });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: error.serverError });
+  }
+};
--- routes/rate.js
+++ routes/rate.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 
 // middleware
+const { authorize } = require("../middleware/auth");
 
 // controllers
+const { getUserToRateProduct } = require("../controller/rate");
 
 // utils
 
 // routes
+router.get("/unrated", getUserToRateProduct);
 
 module.exports = router;
