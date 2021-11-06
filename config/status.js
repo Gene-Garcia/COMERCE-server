6b13ae1bb827867786c24ce9f7ee56b6b8dacc0b
gn-grc <genejogarcia.gg@gmail.com>
2021-11-06 22:10:13 +0800

    definition of order status

--- config/status.js
+++ config/status.js
@@ -1,1 +1,7 @@
+/*
+ * PLACED - default status aftering placing the order
+ * Logistics - status set when the seller ships the product out for delivery
+ * REVIEW - status when the order is received by the customer. The product is now pending for user review
+ * FULFILLED - status after having the ordered product reviewed. indicates the end of the product's order
+ */
 exports.orderStatus = ["Placed", "Logistics", "Review", "Fulfilled"];
