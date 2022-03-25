3495a4d593eee287688ac9dc9b5f1418343066ee
gn-grc <genejogarcia.gg@gmail.com>
2022-03-25 13:22:40 +0800

    updated delivery schema model

--- models/delivererModel.js
+++ models/delivererModel.js
@@ -17,16 +17,76 @@ const delivererSchema = Schema({
     required: "Last name is required",
   },
 
-  contactInformation: [
-    {
-      type: Number,
-      required: "Contact information is required",
+  contactInformation: {
+    streetAddress: {
+      type: String,
     },
-  ],
+
+    barangay: {
+      type: String,
+      required: "Barangay is required",
+    },
+
+    cityMunicipality: {
+      type: String,
+      required: "City or municipality is required",
+    },
+
+    province: {
+      type: String,
+      required: "Province is required",
+    },
+
+    primaryNumber: {
+      type: String,
+      required: "Primary number is required",
+      match: ["^09\\d{9}$", "Invalid phone number"],
+    },
+
+    secondaryNumber: {
+      type: String,
+      match: ["^09\\d{9}$", "Invalid phone number"],
+    },
+  },
 
   vehicleInformation: {
-    type: Object,
-    required: "Vehicle information is required",
+    maker: {
+      type: String,
+      required: "Car maker is required",
+    },
+
+    plateNumber: {
+      type: String,
+      required: "Plate number is required",
+      match: ["^\\d{3}[A-Z]{4}$", "Invalid plate number"],
+    },
+
+    classification: {
+      type: String,
+      required: "Car classification is required",
+    },
+
+    registeredOwner: {
+      type: String,
+      required: "Registered owner is required",
+    },
+
+    fuel: {
+      type: String,
+      required: "Car fuel type is required",
+    },
+
+    engineCapacity: {
+      type: Number,
+      required: "Car engine capacity in cubic centimeters is required",
+      min: [150, "150 is the minimum recognized engine capacity"],
+      max: [7000, "7000 is the maximum recognized engine capacity"],
+    },
+
+    transmission: {
+      type: String,
+      required: "Car transmission type is required",
+    },
   },
 });
 
